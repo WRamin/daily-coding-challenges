dcc560c9387a4cb079f02ba245e0f0fbe5480623
WRamin <ramin.w7@outlook.com>
2025-06-02 20:37:49 -0400

    2 code wars questions completed

---
+++ codewars/2025-06-02/Function-2-squaring-an-argument.js
@@ -0,0 +1,15 @@
+// JS fundamental problem, difficulty 8 
+
+//Now you have to write a function that takes an argument and returns the square of it.
+
+function squareF (arg) {
+    return arg ** 2;
+}
+
+//time conplexity is O(1) constant: squaring is a single arithmatic opertation regarless of the size of input.
+//space complexity is O(1) constant: the function only uses a fixed amount of memory to store the input and the output
+
+
+// Another way to do is 
+
+const squareF = (arg) => arg ** 2;
---
+++ codewars/2025-06-02/Reverse-Words.js
@@ -0,0 +1,30 @@
+// string problem, difficulty 7
+
+/*
+Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+Examples
+
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+*/
+
+function reverseStr(str) {
+    // splits the string into words
+    const words = .split(' ')
+    // split and reverse the letters in the words --- map goes through each word
+    const reverseWords = words.map(word => word.split('').reverse().join(''));
+    // rejoin the words into a string
+    const result = reverseWords.join(' ')
+    return result
+}
+
+// time complexity is O(n), every step processes or creates a structure proporational to the input size (n)
+// time complexity is O(n), nothin is reused in place (since strings are immutable in js)
+
+
+//Another Solution to this problems is 
+
+function reverseStr(str) {
+    //split words into seperate arrays
+    return str.split("").reverse().join("").split(" ").reverse().join(" ");
+  }
